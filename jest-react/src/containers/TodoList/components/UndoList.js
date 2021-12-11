c10bd90376dc8544309c337b43b2ab4917032620
xieyan7 <xieyan7@jd.com>
2021-12-11 22:58:22 +0800

    lesson7: UndoList样式修饰及测试代码优化

--- jest-react/src/containers/TodoList/components/UndoList.js
+++ jest-react/src/containers/TodoList/components/UndoList.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
-
+import '../style.css';
 
 class UndoList extends Component {
-  constructor(props){
-    super(props)
-  }
 
   render(){
     const { deleteItem, list } = this.props;
     return(
-      <div>
-        <div data-test="count">{list.length}</div>
-        <ul>
+      <div className="undoList">
+        <div className="undoList-title">
+          <p>正在进行</p>
+          <div data-test="count" className="count">{list.length}</div>
+        </div>
+        <ul className="item-wrap">
           {
             list.map((item, index) => {
               return(
-                <li key={`${item}-${index}`} data-test="list-item">
+                <li key={`${item}-${index}`} data-test="list-item" className="item">
                   {item}
                   <span
+                    className="delBtn"
                     data-test="delete-item"
                     onClick={() => deleteItem(index)}
                   >
