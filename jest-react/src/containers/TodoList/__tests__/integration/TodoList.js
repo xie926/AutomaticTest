0d0c8e06f4b68b64def50c1bfb923aade8396513
xie926 <1187904163@qq.com>
2021-12-14 09:37:36 +0800

    lesson10: 使用BDD和集成测试进行Rddux项目的测试

--- jest-react/src/containers/TodoList/__tests__/integration/TodoList.js
+++ jest-react/src/containers/TodoList/__tests__/integration/TodoList.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import { findTestWrapper } from '../../../../utils/testUtils';
+import { Provider } from 'react-redux';
+import store from '../../../../store/createStore'
 import TodoList from '../../index';
 it(`
   1.Header输入框输入内容
   2.点击回车
   3.列表展示用户的输入项
 `, () => {
-  const wrapper = mount(<TodoList/>);
+  const wrapper = mount(<Provider store={store}><TodoList/></Provider>);
   const inputEle = findTestWrapper(wrapper, 'header-input');
   const value = 'xieyan';
   inputEle.simulate('change', {
---
+++ jest-react/src/store/createStore.js
@@ -0,0 +1,11 @@
+import { combineReducers, createStore } from 'redux';
+import { reducer as todoReducer } from '../containers/TodoList/store';
+
+const reducer = combineReducers({
+    todo: todoReducer
+})
+
+const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+export default store
+
